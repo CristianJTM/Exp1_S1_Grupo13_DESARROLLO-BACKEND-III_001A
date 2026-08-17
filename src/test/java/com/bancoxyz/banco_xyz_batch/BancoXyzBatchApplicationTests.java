20cbfabb28a533b943369aa24f9a1e9017933430
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-08-17 08:04:33 -0400

    initialize Spring Boot project with Maven setup and basic configurations

---
+++ src/test/java/com/bancoxyz/banco_xyz_batch/BancoXyzBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoxyz.banco_xyz_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoXyzBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
